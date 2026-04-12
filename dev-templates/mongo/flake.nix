34e9da44629714aeb0f554ba0a5e3d2b0feca37e
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-04-12 03:02:26 -0600

    add: flake for mongodb tweaking

---
+++ dev-templates/mongo/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Python template: FastAPI";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          mongosh
+          mongodb-tools
+        ];
+      };
+    };
+}
