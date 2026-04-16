50a7c3e2a7a1bec21bb5bcbae3430bbc0a9e0ac9
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-04-16 16:53:50 -0600

    add macbook stuff

--- nixos-dotfiles/hosts.nix
+++ nixos-dotfiles/hosts.nix
@@ -11,4 +11,10 @@
     hardwareConfig = ./hardware-configuration.thinkpad.nix;
     hardwareModules = [ nixos-hardware.nixosModules.lenovo-thinkpad-x13-intel ];
   };
+
+  macbook = {
+    hostname = "macbook-pro";
+    hardwareConfig = ./hardware-configuration.macbook.nix;
+    hardwareModules = [ nixos-hardware.nixosModules.apple-macbook-pro-14-1 ];
+  };
 }
